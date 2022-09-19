b9f31360024392fa9e891e9ba31dbc2b5232feae
raziq adeel <raziq.adeel@ibm.com>
2022-09-19 10:32:57 +0200

    user and role is added

--- src/main/java/Coworking/Api/userController.java
+++ src/main/java/Coworking/Api/userController.java
@@ -4,9 +4,7 @@ import Coworking.Coworkingspace.User.User;
 import Coworking.Services.UserServices;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,5 +18,9 @@ public class userController {
     @GetMapping("/users")
     public ResponseEntity<List<User>>getUser(){
         return ResponseEntity.ok().body(userServices.getUser());
+    }
+    @PostMapping("/user/save")
+    public  ResponseEntity<User>saveUser(@RequestBody User user)    {
+        return ResponseEntity.ok().body(userServices.saveUser(user));
     }
 }
